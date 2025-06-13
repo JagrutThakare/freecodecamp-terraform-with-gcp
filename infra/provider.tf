db8934cbd54b36cc4a7d99ee6da019ae25b041e6
Jagrut Thakare <114098488+JagrutThakare@users.noreply.github.com>
2025-06-13 15:54:21 +0530

    v1

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,10 +5,3 @@ provider "google" {
   project      = var.gcp_project
   region       = var.gcp_region
 }
-
-# GCP beta provider
-provider "google-beta" {
-  credentials  = file(var.gcp_svc_key)
-  project      = var.gcp_project
-  region       = var.gcp_region
-}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,11 @@
-variable "gcp_svc_key" {}
-variable "gcp_project" {}
-variable "gcp_region" {}
+variable "gcp_svc_key" {
+
+}
+
+variable "gcp_project" {
+
+}
+
+variable "gcp_region" {
+
+}
